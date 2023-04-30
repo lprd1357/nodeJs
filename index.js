4054d469721636c03ee2be22da2ffdb75d8c064e
lprd1357 <39974704+lprd1357@users.noreply.github.com>
2023-04-30 21:09:12 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var figlet = require("figlet");
+
+// figlet("nTerarosa", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+//   disData = data;
+// });
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/figlet/:name', (req, res)=>{
+  const { name } = req.params;
+
+  //figlet : 문자를 그림으로
+  figlet(name, function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    //console.log("figlet >>>> " + data +"<<<<<<");
+    console.log(`figlet >>>> ${data} <<<<<<`);
+    console.log(data);
+    res.send('<pre><font face="Courier New">'+data+'</font></pre>');
+    //res.send(disData);
+  });
+
+
+})
+
+app.listen(port, ()=>{
+  console.log(`port is ${port}`);
+})
