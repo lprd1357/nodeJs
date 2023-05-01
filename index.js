c03c4d9c9af3002e16b49df05fb56ac05411ea01
lprd1357 <39974704+lprd1357@users.noreply.github.com>
2023-05-01 13:56:33 +0900

    Add files via upload
    2차 업로드

--- index.js
+++ index.js
@@ -11,10 +11,20 @@ var figlet = require("figlet");
 // });
 
 const express = require('express');
+const cors  = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
+// app.use(express.json())
+// app.use(express.urlencoded({ extends: true}))
+
+app.listen(port, ()=>{
+  console.log(`port is ${port}`);
+})
+
 app.get('/figlet/:name', (req, res)=>{
+  
   const { name } = req.params;
 
   //figlet : 문자를 그림으로
@@ -25,15 +35,33 @@ app.get('/figlet/:name', (req, res)=>{
       return;
     }
     //console.log("figlet >>>> " + data +"<<<<<<");
-    console.log(`figlet >>>> ${data} <<<<<<`);
+    console.log(`figlet GET >>>> ${data} <<<<<<`);
     console.log(data);
     res.send('<pre><font face="Courier New">'+data+'</font></pre>');
-    //res.send(disData);
+    //res.send(data);
+    //res.json({'data' : data});
   });
-
 
 })
 
-app.listen(port, ()=>{
-  console.log(`port is ${port}`);
+app.post('/', (req, res)=>{
+  console.log(` [POST]`);
+  console.log(` req.body >>> ${req.body} <<<`);
+  const { name } = req.body;
+
+  //figlet : 문자를 그림으로
+  figlet(name, function (err, data) {
+    if (err) {
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
+    }
+    //console.log("figlet >>>> " + data +"<<<<<<");
+    console.log(`figlet POST >>>> ${data} <<<<<<`);
+    console.log(data);
+    //res.send('<pre><font face="Courier New">'+data+'</font></pre>');
+    //res.send(data);
+    res.json({'data' : data});
+  });
+
 })
